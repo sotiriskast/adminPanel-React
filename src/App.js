22d9ae29cc5868965459d84cf4552f5f326b8f94
Autocommit <autocommit@ideaseven.com>
2022-07-15 14:52:54 +0300

    r

--- src/App.js
+++ src/App.js
@@ -2,8 +2,25 @@ import React, {useEffect} from "react";
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import {FiSettings} from 'react-icons/fi';
 import {TooltipComponent} from "@syncfusion/ej2-react-popups";
-import {Navbar,Footer,Sidebar,ThemeSettings} from "./components";
-import {Ecommerce,Orders,Calendar,Employees,Stacked,Pyramid,Customers,Kanban,Area,Bar,Pie,Line,Financial,ColorMapping,ColorPicker,Editor} from "./pages";
+import {Navbar, Footer, Sidebar, ThemeSettings} from "./components";
+import {
+    Ecommerce,
+    Orders,
+    Calendar,
+    Employees,
+    Stacked,
+    Pyramid,
+    Customers,
+    Kanban,
+    Area,
+    Bar,
+    Pie,
+    Line,
+    Financial,
+    ColorMapping,
+    ColorPicker,
+    Editor
+} from "./pages";
 import {useStateContent} from "./contexts/ContextProvider";
 
 import './App.css'
@@ -36,32 +53,32 @@ const App = () => {
                         `dark:bg-main-bg bg-main-bg min-h-screen ${activeMenu ? 'md:ml-72' : 'flex-2'}`
                     }>
                         <div className={'fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'}>
-                            
+                            <Navbar/>
                         </div>
                     </div>
                     <div>
                         <Routes>
                             {/*Dashboard*/}
-                            <Route path={'/'} element={<Ecommerce/>} />
-                            <Route path={'/ecommerce'} element={<Ecommerce/>} />
+                            <Route path={'/'} element={<Ecommerce/>}/>
+                            <Route path={'/ecommerce'} element={<Ecommerce/>}/>
                             {/*Pages*/}
-                            <Route path={'/orders'} element={<Orders/>} />
-                            <Route path={'/employees'} element={<Employees/>} />
-                            <Route path={'/customers'} element={<Customers/>} />
+                            <Route path={'/orders'} element={<Orders/>}/>
+                            <Route path={'/employees'} element={<Employees/>}/>
+                            <Route path={'/customers'} element={<Customers/>}/>
                             {/*App*/}
-                            <Route path={'/kanban'} element={<Kanban/>} />
-                            <Route path={'/editor'} element={<Editor/>} />
-                            <Route path={'/calendar'} element={<Calendar/>} />
-                            <Route path={'/color-picker'} element={<ColorPicker/>} />
+                            <Route path={'/kanban'} element={<Kanban/>}/>
+                            <Route path={'/editor'} element={<Editor/>}/>
+                            <Route path={'/calendar'} element={<Calendar/>}/>
+                            <Route path={'/color-picker'} element={<ColorPicker/>}/>
                             {/*Charts*/}
-                            <Route path={'/line'} element={<Line/>} />
-                            <Route path={'/area'} element={<Area/>} />
-                            <Route path={'/bar'} element={<Bar/>} />
-                            <Route path={'/pie'} element={<Pie/>} />
-                            <Route path={'/financial'} element={<Financial/>} />
-                            <Route path={'/color-mapping'} element={<ColorMapping/>} />
-                            <Route path={'/pyramid'} element={<Pyramid/>} />
-                            <Route path={'/stacked'} element={<Stacked/>} />
+                            <Route path={'/line'} element={<Line/>}/>
+                            <Route path={'/area'} element={<Area/>}/>
+                            <Route path={'/bar'} element={<Bar/>}/>
+                            <Route path={'/pie'} element={<Pie/>}/>
+                            <Route path={'/financial'} element={<Financial/>}/>
+                            <Route path={'/color-mapping'} element={<ColorMapping/>}/>
+                            <Route path={'/pyramid'} element={<Pyramid/>}/>
+                            <Route path={'/stacked'} element={<Stacked/>}/>
 
                         </Routes>
                     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import {AiOutlineMenu} from "react-icons/ai";
+import {FiShoppingCart} from "react-icons/fi";
+import {BsChatLeft} from "react-icons/bs";
+import {RiNotification3Line} from "react-icons/all";
+import {TooltipComponent} from "@syncfusion/ej2-react-popups";
+import avatar from '../data/avatar.jpg';
+import {cart, Chat, Notification, UserProfile} from ".";
+import {useStateContent} from "../contexts/ContextProvider";
+
+const NavButton = ({title, customFunc, icon, color, dotColor}) => (
+    <TooltipComponent content={title} position={"BottomCenter"}>
+        <button>
+
+        </button>
+    </TooltipComponent>
+)
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+    const {activeMenu, setActiveMenu} = useStateContent()
+    return (
+        <div className={"flex justify-betweenp-2 md:mx-6 relative"}>
+            <NavButton title={"Menu"} customFunc={()=>setActiveMenu((prevActiveMenu)=>!prevActiveMenu)} color={'blue'} icon={<AiOutlineMenu/>} />
+        </div>
+    )
 }
 
 export default Navbar
